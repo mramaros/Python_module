0a59a7b073d53603e7d4dbf4285eaf908ef33a37
Mramaros <mramaros@student.42antananarivo.mg>
2026-04-13 17:25:11 +0200

    edit

--- python_module01/ex6/ft_garden_analytics.py
+++ python_module01/ex6/ft_garden_analytics.py
@@ -6,7 +6,7 @@
 #    By: mramaros <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/04/12 23:08:00 by mramaros          #+#    #+#              #
-#    Updated: 2026/04/13 08:35:02 by mramaros         ###   ########.fr        #
+#    Updated: 2026/04/13 17:24:59 by mramaros         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -91,4 +91,68 @@ class Tree(Plant):
         self._trunk_diameter = float(trunk_diameter)
         self._stats = Tree.Stats()
 
+    def produce_shade(self)-> None:
+        self._stats._shade_calls += 1
+        print(f"Tree {self._name} now produce a shade of "
+              f"{round(self._height, 1)}cm long and "
+              f"{round(self._trunk_diameter, 1)}cm wide")
+
+    def show(self)->None:
+        super().show()
+        print(f"Trunk diameter: {round(self._trunk_diameter, 1)}cm")
+
+Class Seed(Flower):
+    def __init__(self, name, height, age, color, seeds=0):
+        super().__init__(name, height, age, color)
+        self._seeds = seeds
+
+    def show(self)-> None:
+        super().show()
+        print(f"Seeds: {self._seeds}")
+
+Class Vegetable(Plant):
+    def __init__(self, name, age, height, harvest_season):
+        super().__init__(name, age, height)
+        self._harvest_season = harvest_season
+        self._nutrition_value = 0
+
+    def grow(self, amount=1.0)-> None:
+        super().grow(amount)
+
+    def age(self)-> None:
+        super().age(days)
+        print(f"Harvest_season: {self._harvest_season}")
+        print(f"Nutrition_value: {self._nutrition_value}")
+
+def display_stats(Plant):
+    print(f"[statistic for {self._name}]")
+    plant._stats.display()
+
+if __name__ == "__main__":
+    print("=== Garden Statics ===")
+    print("\n===Check year old")
+    print("Is 30 days more than a year? -> "
+          f"{Plant.is_older_than_a_year(30)}")
+    print("Is 400 days than a year? -> "
+          f"{Plant.is_older_than_a_year(400)}")
+
+    print("\n===Flower")
+    rose = Flower("Rose", 15, 10, "red")
+    rose.show()
+    display_stats(rose)
+    print("[asking the rose to grow and bloom]")
+    rose.grow(8)
+    rose.bloom()
+    display_stats(rose)
+
+    print("\n=== Tree")
+    oak = Tree("Oak", 200 , 365, 5)
+    oak.show()
+    display_stats(oak)
+    print("[Asking the oak to produce the shade]")
+    oak.produce_shade()
+    display_stats(oak)
+
+    print("\n=== Seed")
+    sunflower = Seed("Sunflower", 80.0, 45, "Yellow")
 
