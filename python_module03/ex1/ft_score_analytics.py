bb069f598ea410b303a7fa25923adc609ef968d4
mramaros <mramaros@mramaros.mramaro42>
2026-04-23 19:27:00 +0200

    edit

--- python_module03/ex1/ft_score_analytics.py
+++ python_module03/ex1/ft_score_analytics.py
@@ -2,20 +2,45 @@
 
 import sys
 
+
 def main() -> None:
     print("=== Player Score Analytics ===")
 
     raw_argv = sys.argv[1:]
 
     if not raw_argv:
-        print("No scores provided. Usage: python3" 
-            "ft_score_analytics.py <score1> <score2> ...")
-    
+        print("No scores provided. Usage: python3", end="")
+        print("ft_score_analytics.py <score1> <score2> ...")
+        return
+
     score: list[int] = []
 
-    for  i in raw_argv:
+    for i in raw_argv:
         try:
-            score = int()
+            input = int(i)
+            score.append(input)
+        except ValueError:
+            print(f"Invalid parameter: '{i}'")
+
+    if not score:
+        print("No scores provided.", end="")
+        print(" Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+
+    else:
+        total_player = len(score)
+        total_score = sum(score)
+        average_score = total_score / total_player
+        high_score = max(score)
+        low_score = min(score)
+        score_range = high_score - low_score
+
+        print(f"Scores processed: {score}")
+        print(f"Total players: {total_player}")
+        print(f"Total score: {total_score}")
+        print(f"Average score: {average_score}")
+        print(f"High score: {high_score}")
+        print(f"Low score: {low_score}")
+        print(f"Score_range: {score_range}")
 
 
 if __name__ == "__main__":
