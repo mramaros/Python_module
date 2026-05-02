53d02af206a58dbfcf35fa658d5a9e8d88e11f6f
Manda Heriniaina Ramarosandratana <mramaros@c1r11s11.42antananarivo.mg>
2026-05-02 06:50:39 +0300

    fina_edit

---
+++ python_module03/ex6/ft_data_alchemist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import random
+
+
+def main() -> None:
+    print("=== Game Data Alchemist ===")
+
+    players: list[str] = {
+            "Alice", "bob", "Charlie", "dylan", "Emma", "Gregory", "john", "kevin", "Liam"
+            }
+
+    print(f"Initial list of players: {players}")
+
+    all_capitalized = [name.capitalized() for in ]
+
+
+if __name__ == "__main__":
+    main()
