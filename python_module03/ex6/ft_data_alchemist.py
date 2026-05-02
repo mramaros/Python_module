536b97f7ec19f419ee7c9438cac3f24ac29780ac
mramaros <mramaros@mramaros.mramaro42>
2026-05-02 06:46:29 +0200

    python_module03

--- python_module03/ex6/ft_data_alchemist.py
+++ python_module03/ex6/ft_data_alchemist.py
@@ -12,7 +12,26 @@ def main() -> None:
 
     print(f"Initial list of players: {players}")
 
-    all_capitalized = [name.capitalized() for in ]
+    all_capitalized = [name.capitalize() for name in players]
+
+    print(f"New list for capitalized only: {all_capitalized}")
+
+    all_nocapitalized = [name for name in players if name[0].isupper()]
+
+    print(f"New list of capitalized names only: {all_nocapitalized}")
+
+    score = {name: random.randint(0, 1000) for name in all_capitalized}
+    print(f"Score dict: {score}")
+    
+    average = sum(score.values()) / len(score)
+    print(f"Score average is: {average:.2f}")
+
+    high_score = {}
+    for name, scores in score.items():
+        if scores > average:
+            high_score[name] = scores
+
+    print(f"High scores: {high_score}")
 
 
 if __name__ == "__main__":
